addee0630458ce539c8f6a3f1fdfb314cd5d154d
Jai Malani <malanijai05@gmail.com>
2023-07-06 16:09:06 +0530

    updated headers

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.use(
   cors({
     origin: /\.printcoder\.com$/, // Regex pattern for your domain and all its subdomains
     methods: ["GET", "POST", "PUT", "DELETE"], // Specify the allowed HTTP methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Specify the allowed headers
   }),
 );
 app.use(express.json());
